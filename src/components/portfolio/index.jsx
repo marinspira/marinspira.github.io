0f14ad2c2ce9dd68ad4ccda3cea2af86b909be8d
Maria Eduarda Ferreira <maria.ferreira@feedz.com.br>
2024-07-21 04:31:02 +0700

    Include tecnologies icon

--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -14,9 +14,9 @@ function Portfolio() {
     const projects = [
         {
             img: feedz,
-            name: 'Next.js Website',
+            name: 'Next.js Website (SSR)',
             describe: 'Website developed for Feedz using Next.js, features easy componentization for A/B testing and conversion strategies. It includes CRM integrations with Axios, custom monitoring tags for personalized data analysis, HR logic calculators, and adaptive content based on user UTM parameters.',
-            skills: ['React.js, Next.js'],
+            skills: ['react,nextjs,nodejs,bitbucket'],
             link1: 'https://feedz.com.br',
             link2: 'https://drive.google.com/file/d/1XOJWncAdvyoGoGa93k7qq2l9a4_Gl5WH/view?usp=sharing',
             textLink1: 'Live',
@@ -25,15 +25,15 @@ function Portfolio() {
         },
         {
             img: nhall,
-            name: 'Front-end e-commerce platform using React',
+            name: 'Front-end e-commerce platform using React and Typescript',
             describe: 'The front-end for Nhall Resortwear e-commerce was developed using React and libraries such as Axios, React Router for navigation, Redux for state management, Styled Components for dynamic styling, and React Hook Form. The project offers a modern and responsive interface, designed to deliver an engaging and intuitive shopping experience. Its robust architecture supports easy integration with various backend services.',
-            skills: ['React, Wordpress, Woocommerce'],
+            skills: ['react,typescript'],
         },
         {
             img: expo,
             name: 'React Native App with Express and MongoDB',
             describe: 'A guest check-in application developed in React Native using Expo, with a backend built on Express.js. Schemas and simple methods were created to securely send data to MongoDB. A secure tunnel was configured behind NATs and firewalls using Ngrok for testing on other devices. Libraries such as react-native-image-picker were used for native interaction with the user gallery, and the backend was responsible for saving and modifying images in the database as base64.',
-            skills: ['React.js, Next.js'],
+            skills: ['react,nodejs,express,mongodb'],
             link1: 'https://github.com/marinspira/checkinApp',
             textLink1: 'Repository',
             switchCol: true,
@@ -44,13 +44,13 @@ function Portfolio() {
             img: feedzday,
             name: 'Next.js Website',
             describe: 'Created with Next.js, this site adheres closely to Figma designs to ensure a visually consistent and engaging event experience. It integrates seamlessly with a CRM API for efficient attendee management and personalized communication.',
-            skills: ['React.js, Next.js'],
+            skills: ['react,nextjs'],
         },
         {
             img: go,
-            name: 'Dashboard with Next.js',
+            name: 'Dashboard with Next.js and Typescript',
             describe: 'Created with Next.js, this site adheres closely to Figma designs to ensure a visually consistent and engaging event experience. It integrates seamlessly with a CRM API for efficient attendee management and personalized communication.',
-            skills: ['React.js, Next.js'],
+            skills: ['react,typescript'],
             gif: true,
             switchCol: true
         },
@@ -82,6 +82,9 @@ function Portfolio() {
                     </C.ContentImage>
                     <C.Content>
                         <h3>{project.name}</h3>
+                        {(project.skills).map((skill) => {
+                            return <img src={`https://skillicons.dev/icons?i=${skill}`} />
+                        })}
                         <span>{project.describe}</span>
                         {project.link1 &&
                             <Btn
